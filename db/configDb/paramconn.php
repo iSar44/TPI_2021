152780b715b3936fe3872a89790c68e21b83cbf5
iSar44 <saroukhanian.iliya@gmail.com>
2021-04-29 07:16:58 +0200

    constants for db added

---
+++ db/configDb/paramconn.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author M. Dominique Aigroz
+ */
+
+/**
+ * @remark	Remplir correctement les constantes ci-dessous en fonction de votre base de données
+ */
+define('DATABASE_DBTYPE', "mysql");
+define('DATABASE_DBNAME', "baratie");
+define('DATABASE_HOST', "127.0.0.1");
+define('DATABASE_PORT', "3306");
+define('DATABASE_USER', "root");
+define('DATABASE_PASS', "admin");
