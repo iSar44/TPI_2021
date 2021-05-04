a9739712928c39373568f424fcf5d1c05034e1b8
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-04 10:14:08 +0200

    modif de la barre de navigation

--- src/public/views/struct/nav.php
+++ src/public/views/struct/nav.php
@@ -1,15 +1,14 @@
 <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-    <div class="container-fluid"><a class="navbar-brand" href="#" style="font-size: 30px;">tournamentManager</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navcol-1">
+    <div class="container-fluid"><a class="navbar-brand" href="#" style="font-size: 40px;">tournamentManager</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1" style="margin-top:10px;font-size:20px;">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="#">Accueil</a></li>
-                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Dropdown </a>
-                    <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Plus</a>
+                    <div class="dropdown-menu"><a class="dropdown-item" href="#">K-Admin</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
                 </li>
             </ul>
             <form class="d-flex me-auto navbar-form" target="_self">
-                <div class="d-flex align-items-center"><label class="form-label d-flex mb-0" for="search-field"><i class="bi bi-search"></i></label><input class="form-control search-field" type="search" id="search-field" name="search" /></div>
-            </form><span class="navbar-text"><a class="login" href="login.html">Log In</a></span><a class="btn btn-light action-button" role="button" href="signup.html">Sign Up</a>
+            </form><span class="navbar-text"><a class="login" href="login.html">Log In</a></span><a class="btn btn-light action-button" style="font-size:inherit;" role="button" href="signup.html">Sign Up</a>
         </div>
     </div>
 </nav>
