941bb5d69ba24a17c0d4a5cef0673fd454262e6b
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-04 15:37:24 +0200

    ajout de la redirection vers la page d'authentification

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
+require_once('./src/model/classes/session.php');
 
-$request = $_SERVER['REQUEST_URI'];
+$session = Session::getInstance();
 
-switch ($request) {
-    case $request:
+//$request = $_SERVER['REQUEST_URI'];
+
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+
+switch ($action) {
+    case '':
         require __DIR__ . '/src/public/views/home.php';
         break;
+    case 'login':
+        require __DIR__ . '/src/public/views/login.php';
     default:
         http_response_code(404);
         break;
