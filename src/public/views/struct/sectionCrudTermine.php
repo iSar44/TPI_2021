7e8f14be20d338e61071be992f7ddbe6c77b684b
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-04 15:43:35 +0200

    ajout du crud pour les tournois terminés

---
+++ src/public/views/struct/sectionCrudTermine.php
@@ -0,0 +1,51 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-6">
+            <h3 class="text-dark mb-4">Tournois terminés:</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="table-responsive" id="tabContainer">
+                <table class="table table-striped table tablesorter" id="ipi-table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th class="text-center">Nom du tournoi</th>
+                            <th class="text-center">Nombre d&#39;equipes</th>
+                            <th class="text-center">Date/heure du démarrage</th>
+                            <th class="text-center">Statut</th>
+                            <th class="text-center">Description</th>
+                            <th class="text-center filter-false sorter-false">action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        <tr>
+                            <td>Ana</td>
+                            <td>Diseñador</td>
+                            <td>Diseño</td>
+                            <td>Text</td>
+                            <td>Text</td>
+                            <td class="text-center">
+                                <a class="btn btn-primary" role="button" style="  margin: 2px;"><i class="bi bi-eye-fill"></i></a>
+                                <a class="btn btn-success" role="button" style="  background: rgb(11,171,56); margin: 2px;"><i class="bi bi-pencil-fill"></i></a>
+                                <a class="btn btn-danger" role="button" style="  margin: 2px;"><i class="bi bi-trash-fill"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Fer<br /></td>
+                            <td>Desarrollador</td>
+                            <td>Development</td>
+                            <td>Text</td>
+                            <td>Text</td>
+                            <td class="text-center">
+                                <a class="btn btn-primary" role="button" style="  margin: 2px;"><i class="bi bi-eye-fill"></i></a>
+                                <a class="btn btn-success" role="button" style="  background: rgb(11,171,56); margin: 2px;"><i class="bi bi-pencil-fill"></i></a>
+                                <a class="btn btn-danger" role="button" style="  margin: 2px;"><i class="bi bi-trash-fill"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
