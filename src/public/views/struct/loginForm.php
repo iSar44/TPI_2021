834a4bb418ff3387c7af3c7d1a07fe8e92beffb7
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-04 15:42:28 +0200

    création du formulaire d'authentification

---
+++ src/public/views/struct/loginForm.php
@@ -0,0 +1,16 @@
+<form method="post">
+    <h2 class="visually-hidden">Login Form</h2>
+    <div class="illustration">
+        <i class="bi bi-door-open"></i>
+    </div>
+    <div class="mb-3">
+        <input class="form-control" type="text" name="username" placeholder="Email" />
+    </div>
+    <div class="mb-3">
+        <input class="form-control" type="password" name="password" placeholder="Mot de passe" />
+    </div>
+    <div class="mb-3">
+        <input class="btn btn-primary d-block w-100" type="submit" value="Log In" />
+    </div>
+    <a class="forgot" href="#">Forgot your email or password?</a>
+</form>
