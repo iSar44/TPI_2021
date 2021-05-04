12deb636b7ade4d27c0259fdcb283a455cd77b8e
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-04 15:41:48 +0200

    fixé le footer en bas de page

--- src/public/views/struct/footer.php
+++ src/public/views/struct/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer-basic">
+<footer class="footer-basic mt-auto">
     <div class="social"><a href="https://github.com/iSar44"><i class="bi bi-github"></i></a><a href="#"><i class="bi bi-twitter"></i></a><a href="#"><i class="bi bi-facebook"></i></a></div>
     <ul class="list-inline">
         <li class="list-inline-item"><a href="#">Home</a></li>
