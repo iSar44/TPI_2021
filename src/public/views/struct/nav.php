73c7cc4d2c22e0993db524dc6b68ef818fb1224e
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-05 16:33:50 +0200

    formattage plus lisible du code

--- src/public/views/struct/nav.php
+++ src/public/views/struct/nav.php
@@ -1,10 +1,22 @@
 <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-    <div class="container-fluid"><a class="navbar-brand" href="./" style="font-size: 40px;">tournamentManager</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./" style="font-size: 40px;">tournamentManager</a>
+        <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
+            <span class="visually-hidden">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navcol-1" style="margin-top:10px;font-size:20px;">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="./">Accueil</a></li>
-                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Plus</a>
-                    <div class="dropdown-menu"><a class="dropdown-item" href="#">K-Admin</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="./">Accueil</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Plus</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">K-Admin</a>
+                        <a class="dropdown-item" href="#">Second Item</a>
+                        <a class="dropdown-item" href="#">Third Item</a>
+                    </div>
                 </li>
             </ul>
             <form class="d-flex me-auto navbar-form" target="_self">
