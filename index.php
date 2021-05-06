99b038fd854f9fc5c5e07f74f1bf1874dced2ae4
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-06 16:35:39 +0200

    ajout de la page logout au routeur

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 require_once('./src/model/classes/session.php');
 
-$session = Session::getInstance();
-
-//$request = $_SERVER['REQUEST_URI'];
+Session::getInstance();
 
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 
@@ -13,6 +11,10 @@ switch ($action) {
         break;
     case 'login':
         require __DIR__ . '/src/public/views/login.php';
+        break;
+    case 'logout':
+        require __DIR__ . '/src/public/views/logout.php';
+        break;
     default:
         http_response_code(404);
         break;
