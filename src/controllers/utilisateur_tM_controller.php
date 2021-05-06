e799c6302aff7337e42ec416a612cdf3d1772725
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-06 16:37:27 +0200

    ajout de 4 fonctions pour l'authentification d'un utilisateur

--- src/controllers/utilisateur_tM_controller.php
+++ src/controllers/utilisateur_tM_controller.php
@@ -1,7 +1,17 @@
 <?php
+
+/**
+ * @author Iliya Saroukhanian <iliya.srkhn@eduge.ch>
+ * @copyright 2021 Iliya Saroukhanian
+ * @version 1.0.0
+ */
+
 require_once('./src/model/db_model/database.php');
 require_once('./src/model/classes/utilisateur_tM.php');
 
+/**
+ * Contrôleur de la classe Utilisateur_tM
+ */
 class Utilisateur_tM_Controller
 {
     private Utilisateur_tM $user_tM;
@@ -31,6 +41,95 @@ class Utilisateur_tM_Controller
     }
 
 
+
+    public function CheckIfEmailExists($anEmail): bool
+    {
+        $query = Database::prepare("SELECT * FROM UTILISATEUR WHERE `EMAIL` = :EMAIL");
+
+        $query->bindParam(':EMAIL', $anEmail, PDO::PARAM_STR);
+
+        try {
+
+            $query->execute();
+            $userExists = $query->fetch();
+
+            if ($userExists != false) {
+                $userExists = true;
+            }
+
+            return $userExists;
+        } catch (PDOException $e) {
+
+            return false;
+        }
+    }
+
+
+    /**
+     * Fonction qui vérifie si l'email et le mot de passe fournis correspondent à un
+     * enregistrement dans la BDD
+     *
+     * @param string $anEmail
+     * @param string $aPassword
+     * @return boolean
+     */
+    public function CheckIfUserExists($anEmail, $hashedPwd): bool
+    {
+        $query = Database::prepare("SELECT * FROM UTILISATEUR WHERE `EMAIL` = :EMAIL AND `MDP` = :MDP");
+
+        $query->bindParam(':EMAIL', $anEmail, PDO::PARAM_STR);
+        $query->bindParam(':EMAIL', $hashedPwd, PDO::PARAM_STR);
+        try {
+
+            $query->execute();
+            $userExists = $query->fetch();
+
+            return $userExists;
+        } catch (PDOException $e) {
+
+            return false;
+        }
+    }
+
+    public function GetHashPassword($anEmail): string
+    {
+        $query = Database::prepare("SELECT MDP FROM UTILISATEUR WHERE `EMAIL` = :EMAIL");
+
+        $query->bindParam(':EMAIL', $anEmail, PDO::PARAM_STR);
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+
+        try {
+            $query->execute();
+            $queryResult = $query->fetch();
+
+            $pwd = $queryResult['MDP'];
+
+            return $pwd;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function GetNicknameOfUser($anEmail): string
+    {
+        $query = Database::prepare("SELECT `NICKNAME` FROM UTILISATEUR WHERE `EMAIL` = :EMAIL");
+
+        $query->bindParam(':EMAIL', $anEmail, PDO::PARAM_STR);
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+
+        try {
+            $query->execute();
+            $queryResult = $query->fetch();
+
+            $nickname = $queryResult['NICKNAME'];
+
+            return $nickname;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+
     /**
      * Fonction qui retourne un tableau d'objets, chaque objet dans le tableau est un utilisateur
      *
@@ -40,7 +139,7 @@ class Utilisateur_tM_Controller
     {
         $results = array();
 
-        $query = Database::getInstance()->prepare("SELECT * FROM UTILISATEUR");
+        $query = Database::prepare("SELECT `ID`, `NICKNAME`, `EMAIL`, `MDP`, `ADMIN` FROM UTILISATEUR");
         $query->execute();
 
         while ($rowInDb = $query->fetch(PDO::FETCH_ASSOC)) {
