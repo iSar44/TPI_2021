e4657231ce49ba54d42b19d4adc5c01f4f0e00c1
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-06 16:39:24 +0200

    page pour la création d'un tournoi (CRUD)

---
+++ src/public/views/createTournament.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Iliya Saroukhanian <iliya.srkhn@eduge.ch>
+ * @copyright 2021 Iliya Saroukhanian
+ * @version 1.0.0
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
