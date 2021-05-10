02ad434f882cb0a625cf06d4a1016db9df406d08
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-10 07:56:56 +0200

    on cache certaines pages si l'on n'est pas connecté

--- src/public/views/struct/nav.php
+++ src/public/views/struct/nav.php
@@ -10,18 +10,20 @@
                 <li class="nav-item">
                     <a class="nav-link" href="./">Accueil</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Plus</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">K-Admin</a>
-                        <a class="dropdown-item" href="#">Second Item</a>
-                        <a class="dropdown-item" href="#">Third Item</a>
-                    </div>
-                </li>
+                <?php if (isset($_SESSION['isLoggedIn'])) : ?>
+                    <li class="nav-item dropdown">
+                        <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Plus</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="./?action=create">Créer un tournoi</a>
+                            <a class="dropdown-item" href="#">Second Item</a>
+                            <a class="dropdown-item" href="#">Third Item</a>
+                        </div>
+                    </li>
+                <?php endif; ?>
             </ul>
             <form class="d-flex me-auto navbar-form" target="_self">
             </form>
-            <?php if ($_SESSION['isLoggedIn'] == false) : ?>
+            <?php if (!isset($_SESSION['isLoggedIn'])) : ?>
                 <a class="btn btn-light action-button" style="font-size:inherit;" role="button" href="./?action=login">Log In</a>
             <?php else : ?>
                 <a class="navbar-brand" style="font-size: 20px;" disabled><?= $_SESSION['username']; ?></a>
