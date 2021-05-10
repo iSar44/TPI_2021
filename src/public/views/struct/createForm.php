9075e7abe4edb5e13f47c620d10ed295dc3900e9
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-10 07:56:20 +0200

    formulaire pour la création de tournoi

---
+++ src/public/views/struct/createForm.php
@@ -0,0 +1,80 @@
+<form method="POST" action="#">
+    <div class="form-group mb-3">
+        <div id="formdiv">
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Name </strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><input type="text" class="form-control" style="margin-left:0px;font-family:Roboto, sans-serif;" name="name" placeholder="Name" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;margin-top:-16px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Gender </strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><select class="form-select" style="font-family:Roboto, sans-serif;" name="gender">
+                        <optgroup label="Gender">
+                            <option value>Male</option>
+                            <option value>Female</option>
+                        </optgroup>
+                    </select></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Birth Date</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><input class="form-control" style="font-family:Roboto, sans-serif;" name="birthDate" type="date" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Birth Place</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><input type="text" class="form-control" style="margin-left:0px;font-family:Roboto, sans-serif;" name="birthPlace" placeholder="Place" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Mother&#39;s Name</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1" style="font-family:Roboto, sans-serif;"><input type="text" class="form-control" style="margin-left:0px;" placeholder="Name" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Father&#39;s Name</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><input type="text" class="form-control" style="margin-left:0px;font-family:Roboto, sans-serif;" placeholder="Name" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Birth Place Address</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><textarea class="form-control" style="font-family:Roboto, sans-serif;" placeholder="Address"></textarea></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Permenent Address</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><textarea class="form-control" style="font-family:Roboto, sans-serif;" placeholder="Address"></textarea></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Register Number</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><input type="number" class="form-control" style="font-family:Roboto, sans-serif;" placeholder="Number" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Register Date</strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><input class="form-control" style="font-family:Roboto, sans-serif;" type="date" /></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-md-8 offset-md-1">
+                    <p style="margin-left:2%;font-family:Roboto, sans-serif;font-size:24px;"><strong>Remarks </strong></p>
+                </div>
+                <div class="col-md-10 offset-md-1"><textarea class="form-control" style="font-family:Roboto, sans-serif;font-size:14px;" placeholder="Remarks"></textarea></div>
+            </div>
+            <div class="row" style="margin-right:0px;margin-left:0px;padding-top:24px;">
+                <div class="col-12 col-md-4 offset-md-4"><button class="btn btn-light btn-lg" style="font-family:Roboto, sans-serif;" type="reset">Clear </button><button class="btn btn-light btn-lg" style="margin-left:16px;" type="submit">Submit </button></div>
+            </div>
+        </div>
+    </div>
+</form>
