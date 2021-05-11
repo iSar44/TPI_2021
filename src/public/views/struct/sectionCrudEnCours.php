2b5b4fb76d8d5bdec88700f480b1559cc3d7b60b
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-11 11:16:46 +0200

    affichage correct du CRUD

--- src/public/views/struct/sectionCrudEnCours.php
+++ src/public/views/struct/sectionCrudEnCours.php
@@ -8,30 +8,35 @@
     <div class="row">
         <div class="col-12">
             <div class="table-responsive" id="tabContainer">
-                <table class="table table-striped table tablesorter" id="ipi-table">
+                <table class="table table-hover table-bordered table-striped table tablesorter" id="ipi-table">
                     <thead class="thead-dark">
                         <tr>
                             <th class="text-center">Nom du tournoi</th>
+                            <th class="text-center">Description</th>
                             <th class="text-center">Nombre d&#39;equipes</th>
                             <th class="text-center">Date/heure du démarrage</th>
                             <th class="text-center">Statut</th>
-                            <th class="text-center">Description</th>
                             <th class="text-center filter-false sorter-false">action</th>
                         </tr>
                     </thead>
                     <tbody class="text-center">
                         <?php
-                        foreach ($res as $attr) {
+                        foreach ($allTournaments as $aTournament) {
+                            $statut = (strtotime($aTournament->getDateHeureDemarrage()) < $currentDate) ? "<td><h5>En cours</h5></td>" :  "<td><h5>A venir</h5></td>";
+                            $displayDate = date("d/m/Y H:i:s", strtotime($aTournament->getDateHeureDemarrage()));
                             echo "<tr>";
-                            echo "<td>" . $attr->getTitre() . "</td>";
-                            echo "<td>" . $attr->getNbEquipes() . "</td>";
-                            echo "<td>" . $attr->getDateHeureDemarrage() . "</td>";
-                            echo "<td>X</td>";
-                            echo "<td>" . $attr->getDescription() . "</td>";
+                            echo "<td><h5>" . $aTournament->getTitre() . "</h5></td>";
+                            echo "<td><h5>" . $aTournament->getDescription() . "</h5></td>";
+                            echo "<td><h5>" . $aTournament->getNbEquipes() . "</h5></td>";
+                            echo "<td><h5>" . $displayDate . "</h5></td>";
+                            // ($attr->getDateHeureDemarrage() < $currentDate) ? "<td>En cours</td>" :  "<td>A venir</td>";
+                            echo $statut;
                             echo "<td class='text-center'>";
                             echo "<a class='btn btn-primary' role='button' style='margin: 2px;'><i class='bi bi-eye-fill'></i></a>";
-                            echo "<a class='btn btn-success' role='button' style='background: rgb(11,171,56); margin: 2px;'><i class='bi bi-pencil-fill'></i></a>";
-                            echo "<a class='btn btn-danger' role='button' style='margin: 2px;'><i class='bi bi-trash-fill'></i></a>";
+                            if (strtotime($aTournament->getDateHeureDebutInscription()) > $currentDate && isset($_SESSION['isLoggedIn'])) {
+                                echo "<a class='btn btn-success' role='button' style='background: rgb(11,171,56); margin: 2px;' href='./?action=edit&id=" . $aTournament->getId() . "'><i class='bi bi-pencil-fill'></i></a>";
+                                echo "<a class='btn btn-danger' role='button' style='margin: 2px;' href='./?action=delete&id=" . $aTournament->getId() . "'><i class='bi bi-trash-fill'></i></a>";
+                            }
                             echo "</td>";
                             echo "</tr>";
                         }
