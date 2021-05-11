6eb9a0906cab244a2a637484758c8e05b57ef6fd
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-11 16:43:55 +0200

    modifications des test pour l'affichage des pages utilisateur/administrateur

--- src/public/views/struct/nav.php
+++ src/public/views/struct/nav.php
@@ -1,3 +1,8 @@
+<?php
+
+$u_controller = new Utilisateur_tM_Controller();
+
+?>
 <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
     <div class="container-fluid">
         <a class="navbar-brand" href="./" style="font-size: 40px;">tournamentManager</a>
@@ -10,11 +15,11 @@
                 <li class="nav-item">
                     <a class="nav-link" href="./">Accueil</a>
                 </li>
-                <?php if (isset($_SESSION['isLoggedIn'])) : ?>
+                <?php if (isset($u_controller) && $u_controller->CheckIfUserIsAdmin($_SESSION['username']) === 1) : ?>
                     <li class="nav-item dropdown">
                         <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Plus</a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="./?action=create">Créer un tournoi</a>
+                            <a class="dropdown-item" href="./?action=create&admin=true">Créer un tournoi</a>
                             <a class="dropdown-item" href="#">Second Item</a>
                             <a class="dropdown-item" href="#">Third Item</a>
                         </div>
@@ -26,7 +31,7 @@
             <?php if (!isset($_SESSION['isLoggedIn'])) : ?>
                 <a class="btn btn-light action-button" style="font-size:inherit;" role="button" href="./?action=login">Log In</a>
             <?php else : ?>
-                <a class="navbar-brand" style="font-size: 20px;" disabled><?= $_SESSION['username']; ?></a>
+                <a class="navbar-brand" style="font-size: 20px;" disabled><?= (isset($u_controller) && $u_controller->CheckIfUserIsAdmin($_SESSION['username']) === 1) ? $_SESSION['username'] . " -> K-Admin" : $_SESSION['username'] . " -> Player"; ?></a>
                 <a class="btn btn-light action-button" style="font-size:inherit;" role="button" href="./?action=logout">Log Out</a>
             <?php endif; ?>
         </div>
