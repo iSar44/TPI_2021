78cbb05146d45e7f6f4c782f25d0dce4540a0ced
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-12 16:46:37 +0200

    création du contrôleur de l'équipe

---
+++ src/controllers/equipe_tM_controller.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @author Iliya Saroukhanian <iliya.srkhn@eduge.ch>
+ * @copyright 2021 Iliya Saroukhanian
+ * @version 1.0.0
+ */
+
+require_once('./src/web.inc.all.php');
+
+/**
+ * Classe qui représente le contrôleur de la classe Equipe_tM
+ */
+class Equipe_tM_Controller
+{
+
+    private Equipe_tM $equipe_tM;
+
+    /**
+     * Get the value of equipe_tM
+     *
+     * @return Equipe_tM
+     */
+    public function getEquipeTM(): Equipe_tM
+    {
+        return $this->equipe_tM;
+    }
+
+    /**
+     * Set the value of equipe_tM
+     *
+     * @param Equipe_tM $equipe_tM
+     *
+     * @return self
+     */
+    public function setEquipeTM(Equipe_tM $equipe_tM): self
+    {
+        $this->equipe_tM = $equipe_tM;
+
+        return $this;
+    }
+
+    public function SelectAll()
+    {
+        $resultEquipes = array();
+
+        $query = Database::prepare("SELECT `UTILISATEUR_ID`, `NOM_EQUIPE` FROM EQUIPE");
+        $query->execute();
+
+        while ($rowInDb = $query->fetch(PDO::FETCH_ASSOC)) {
+
+            $equipe = new Equipe_tM();
+
+            $equipe->setId($rowInDb['UTILISATEUR_ID']);
+            $equipe->setNomEquipe($rowInDb['NOM_EQUIPE']);
+
+            array_push($resultEquipes, $equipe);
+        }
+
+        return $resultEquipes;
+    }
+
+    public function FindTeam($idUser)
+    {
+        $query = Database::prepare("SELECT `UTILISATEUR_ID`, `NOM_EQUIPE` FROM EQUIPE WHERE UTILISATEUR_ID = :UTILISATEUR_ID");
+
+        $query->bindParam(':UTILISATEUR_ID', $idUser, PDO::PARAM_INT);
+
+        try {
+            $query->execute();
+            if ($rowInDb = $query->fetch()) {
+
+                $equipe = new Equipe_tM();
+
+                $equipe->setId($rowInDb['UTILISATEUR_ID']);
+                $equipe->setNomEquipe($rowInDb['NOM_EQUIPE']);
+
+                return $equipe;
+            }
+        } catch (PDOException $e) {
+            echo "Exception - function FindTeam(idUser): " . $e->getMessage();
+            return false;
+        }
+
+        return false;
+    }
+}
