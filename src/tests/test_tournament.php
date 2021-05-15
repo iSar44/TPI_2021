3318df8bfe63e4a458d477a9c2dce64916191c63
iSar44 <saroukhanian.iliya@gmail.com>
2021-05-15 17:44:50 +0200

    tests supplémentaires

--- src/tests/test_tournament.php
+++ src/tests/test_tournament.php
@@ -13,14 +13,15 @@ $tournoiControl = new Tournoi_tM_Controller();
 $tournoiK = $tournoiControl->SelectTournament(1);
 
 $equipeControl = new Equipe_tM_Controller();
-// $loser = $equipeControl->FindTeam(5);
-// $equipe2 = $equipeControl->FindTeam(3);
-// $winner = $equipeControl->FindTeam(7);
-// $equipe4 = $equipeControl->FindTeam(5);
-// $equipe5 = $equipeControl->FindTeam(6);
-// $equipe6 = $equipeControl->FindTeam(7);
-// $equipe7 = $equipeControl->FindTeam(8);
-// $equipe8 = $equipeControl->FindTeam(9);
+
+$lake2 = $equipeControl->FindTeam(2);
+$river3 = $equipeControl->FindTeam(3);
+$road4 = $equipeControl->FindTeam(4);
+$tree5 = $equipeControl->FindTeam(5);
+$cloud6 = $equipeControl->FindTeam(6);
+$mountain7 = $equipeControl->FindTeam(7);
+$forest8 = $equipeControl->FindTeam(8);
+$house9 = $equipeControl->FindTeam(9);
 
 
 
@@ -68,9 +69,9 @@ $equipeControl = new Equipe_tM_Controller();
 #endregion
 
 #region Test de la fonction CreateAllRoundsForTournament(Tournoi_tM $unTournoi, $nbMatches, $tempsPreparation = "00:00") - TEST PASSED!
-echo "<pre>";
-var_dump($tournoiControl->CreateAllRoundsForTournament($tournoiK, 8, "00:01"));
-echo "</pre>";
+// echo "<pre>";
+// var_dump($tournoiControl->CreateRoundForTournament($tournoiK, 8, "00:01"));
+// echo "</pre>";
 #endregion
 
 #region Test de la fonction CreateMatchTournament(Equipe_tM $team1, Equipe_tM $team2) - TEST PASSED!
@@ -111,7 +112,42 @@ echo "</pre>";
 #endregion
 
 // echo "<pre>";
+// var_dump($tournoiControl::TournamentContinues($tournoiK));
+// echo "</pre>";
+
+// echo "<pre>";
 // $tournoiControl::LoadTournamentTeams($tournoiK);
 // $tournoiControl::LoadTournamentRounds($tournoiK);
 // var_dump($tournoiK);
 // echo "</pre>";
+
+
+#region TEST -> Simulation d'un tournoi complet! (Equipes déjà inscrites)
+
+// //Création de la 1ère ronde
+// $tournoiControl->CreateRoundForTournament($tournoiK, 8);
+
+// //Début du tournoi (1ère ronde)
+// $tournoiControl->StartTournament($tournoiK);
+
+//Résultats de la 1ère ronde
+// $tournoiControl->SetWinner($lake2, $cloud6);
+// $tournoiControl->SetWinner($mountain7, $road4);
+// $tournoiControl->SetWinner($river3, $tree5);
+// $tournoiControl->SetWinner($forest8, $house9);
+
+// //Fin de la 1ème ronde, création et début de la 2ème
+$tournoiControl->StopRound($tournoiK);
+
+// //Résultats de la 2ème ronde
+// $tournoiControl->SetWinner($lake2, $river3);
+// $tournoiControl->SetWinner($forest8, $mountain7);
+// $tournoiControl->SetWinner($road4, $tree5);
+// $tournoiControl->SetWinner($house9, $cloud6);
+
+//Fin de la 2ème ronde, création et début de la 3ème
+$tournoiControl->StopRound($tournoiK);
+
+
+
+#endregion
